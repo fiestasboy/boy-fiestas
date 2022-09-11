f70c04e40219af2ede5c79253fd861b6cfc7c197
Boy-Fiestas <110788641+fiestasboy@users.noreply.github.com>
2022-09-11 16:03:33 -0500

    configuración

--- boyFiestas/settings.py
+++ boyFiestas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xd@%z-_t2xpy*6y_gdkt^5det04i-1!f+rde6o%d9u4p)jwecj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
